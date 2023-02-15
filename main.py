48f01a02a82bab4baba067fb49f71ebcea02c81c
Mput13 <mputilovskij@gmail.com>
2023-02-15 21:03:34 +0500

    не работают стрелки, тк виджет с вводом текста все сигналы перехватывает

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QComboBox
 
-from work_with_api import get_map
+from work_with_api import get_map, get_map_by_request
 
 
 class MyWidget(QMainWindow):
@@ -14,39 +14,63 @@ class MyWidget(QMainWindow):
     type_choosing: QComboBox
     picture: QLabel
 
-    def __init__(self, cords: [float, float], spn: float):
+    def __init__(self, cords: [str, str], spn: str):
         super().__init__()
         self.spn_step = 3
-        self.coord_step = 0.1
+        self.coord_step = 3
         self.spn = spn
         self.cords = cords
         self.map_file = 'map.png'
         self.events = []
         uic.loadUi('main.ui', self)
-        self.find_button.clicked.connect(self.show_picture)
+        self.find_button.clicked.connect(self.find_object)
+        get_map(self.cords, self.spn)
         self.show_picture()
 
     def show_picture(self):
-        get_map(self.cords, self.spn)
         self.pixmap = QPixmap(self.map_file)
         self.picture.setPixmap(self.pixmap)
 
+    def find_object(self):
+        data = get_map_by_request(self.user_request)
+        self.cords = data[0]
+        self.spn = data[1]
+        self.show_picture()
+
     def keyPressEvent(self, event):
+        x = float(self.cords[0])
+        y = float(self.cords[1])
         if event.key() == Qt.Key_PageDown:
             if self.spn - self.spn_step >= 0:
                 self.spn -= self.spn_step
+                get_map(self.cords, self.spn)
+                self.show_picture()
         if event.key() == Qt.Key_PageUp:
             if self.spn + self.spn_step <= 90:
                 self.spn += self.spn_step
-        if event.key() == Qt.Key_F:
-            print(1)
-        print(self.spn)
-        self.show_picture()
+                get_map(self.cords, self.spn)
+                self.show_picture()
+        if event.key() == Qt.Key_Up:
+            y += self.coord_step
+            get_map(self.cords, self.spn)
+            self.show_picture()
+        if event.key() == Qt.Key_Down:
+            y -= self.coord_step
+            get_map(self.cords, self.spn)
+            self.show_picture()
+        if event.key() == Qt.Key_Right:
+            x += self.coord_step
+            get_map(self.cords, self.spn)
+            self.show_picture()
+        if event.key() == Qt.Key_Left:
+            x -= self.coord_step
+            get_map(self.cords, self.spn)
+            self.show_picture()
 
 
 if __name__ == '__main__':
-    start_cords = [20.012, 15.0124]
-    start_spn = 9
+    start_cords = ['20.012', '15.0124']
+    start_spn = '9'
     app = QApplication(sys.argv)
     ex = MyWidget(start_cords, start_spn)
     ex.show()
--- work_with_api.py
+++ work_with_api.py
@@ -2,13 +2,16 @@ from io import BytesIO
 import requests
 import sys
 
+from spn_get import spn_finder
 
-def get_map(cords: [float, float], spn: float):
 
+def get_map(cords: [str, str], spn: str):
+    geocoder_request = \
+        "http://geocode-maps.yandex.ru/1.x"
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     map_params = {
-        "ll": ",".join([str(i) for i in cords]),
-        "spn": ",".join([str(spn), str(spn)]),
+        "ll": ','.join(cords),
+        "spn": f"{spn},{spn}",
         "l": "map"
     }
     response = requests.get(map_api_server, params=map_params)
@@ -21,3 +24,19 @@ def get_map(cords: [float, float], spn: float):
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
+
+
+def get_map_by_request(string_request):
+    list_request = string_request.text().split()
+    geocoder_request = \
+        "http://geocode-maps.yandex.ru/1.x"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": "+".join(list_request),
+        "format": "json"
+    }
+    response = requests.get(geocoder_request, params=geocoder_params).json()
+    cords = response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
+    spn = spn_finder(response)[0]
+    get_map(cords, str(spn))
+    return cords, spn
